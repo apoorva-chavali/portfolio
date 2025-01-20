616fbbe309366574e6131f6f970be454035686d7
apoorva-chavali <apoorvachavali@gmail.com>
2025-01-20 17:25:44 -0500

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // Export static files
-  // basePath: '/apoorva-chavali.github.io', // Replace 'your-repo' with your GitHub repository name
-  // assetPrefix: '/apoorva-chavali.github.io/', // Prefix for assets
-  // trailingSlash: true, // Adds trailing slashes to paths for static exports
 };
 
 export default nextConfig;
